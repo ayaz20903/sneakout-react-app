58338bc006e0803c24b05b89d160263be61b312f
Ayaz Qureshi <ayazqqq03@gmail.com>
2021-03-06 18:57:03 +0530

    page done

--- src/components/Default.jsx
+++ src/components/Default.jsx
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 class Default extends Component {
   render() { 
     return (
-        <div>
-            <h3>
-            page not found
-               
-            </h3>
+        <div className="constainer">
+          <div className="row">
+            <div className="col-10 mx-auto text-center text-title pt-5">
+              <h1 className=" m-3 display-3">
+                ERROR 
+              </h1>
+              <h2> PAGE NOT FOUND </h2>
+            </div>
+          </div>
         </div>
       );
     }
