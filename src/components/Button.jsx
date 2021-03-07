0f7e57d96b04243429daa8322eab5dd1dcc0735b
Ayaz Qureshi <ayazqqq03@gmail.com>
2021-03-07 22:48:50 +0530

    clr-name changed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,15 +5,15 @@ export const ButtonContainer = styled.button`
     font-size: 1.2rem;
     background: transparent;
     border: 0.05rem solid ;
-    border-color:${props => props.cart ? 'gray' : 'var(--mainYello)'};
-    color:${props => props.cart ? 'gray': 'var(--mainYello)'} ;
+    border-color:${props => props.cart ? 'gray' : 'var(--mainYellow)'};
+    color:${props => props.cart ? 'gray': 'var(--mainYellow)'} ;
     border-radius: 0.3rem;
     padding: 0.3rem 0.5rem;
     courser: pointer;
     transition: all 0.3s ease-in-out;
     &:hover{
-        background: ${props => props.cart ? 'gray' : 'var(--mainYello)'};
-        color: ${props => props.cart ? 'var(--mainWhite)' : 'var(--mainBlue)'};
+        background: ${props => props.cart ? 'gray' : 'var(--mainYellow)'};
+        color: ${props => props.cart ? 'var(--mainWhite)' : 'var(--Maroon)'};
     }
     &:focus{
         outline: none;
