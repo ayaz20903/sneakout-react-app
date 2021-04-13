4d10ee77bbabe3843c890d9c9182fa9aad0664f0
Ayaz Qureshi <75674807+ayaz20903@users.noreply.github.com>
2021-04-14 02:35:23 +0530

    icon added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,10 +10,6 @@ class Navbar extends Component {
         return ( 
             <NavWrapper className='navbar navbar-expand-sm navbar-dark px-sm-5'>
 
-            { /*https://www.iconfinder.com/icons/1243689/call_phone_icon
-                Creative Commons (Attribution 3.0 Unported);
-            https://www.iconfinder.com/Makoto_msk */ }
-
             <Link to='/'>
                 <img src={logo} alt='store' className='navbar-brand'/>
             </Link>
